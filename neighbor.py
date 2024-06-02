f03f0cd4fb656c1fe4dca95df335c78f6aa6f3ad
Hao Jin <hjin033@ucr.edu>
2024-06-02 00:04:17 -0700

    Added backward elimination and deleted temporary nearest neighbor functionality test

--- neighbor.py
+++ neighbor.py
@@ -1,4 +1,5 @@
 import math
+import copy
 
 def nearest(data, features):
     correct = 0
@@ -6,15 +7,16 @@ def nearest(data, features):
     guess = 1
     distance = 10
     
+    #Nested for loop to test each data against the entire dataset
     for i in range(len(data)):
         testData  = data[i].split()
         for j in range(len(data)):
             currentData = data[j].split()
             if testData == currentData:
-                continue
+                continue #skip on the data left out for testing
             distance = 0
             for f in features:
-                distance += (float(testData[f]) - float(currentData[f])) ** 2
+                distance += (float(testData[f]) - float(currentData[f])) ** 2 #Euclidean distance
             distance = math.sqrt(distance)
             if distance < closestDist:
                 closestDist = distance
@@ -28,22 +30,87 @@ def nearest(data, features):
 def forward(fileName):
     file = open(fileName, "r")
     data = file.readlines()
-    featureNum = len(data[0].split()) - 1
+    featureNum = len(data[0].split())
     features = []
     bestFeatures = []
     bestAcc = 0
+    acc = 0
     
-    for i in range(0, featureNum):
+    print("The dataset has %d features and %d instances." % (featureNum - 1, len(data)))
+    print("")
+    
+    #Nested for loop that expand(add feature) upon the (local)best feature set
+    for i in range(1, featureNum):
         currBest = 0
-        for j in range(i, featureNum):
-            currBest = max(nearest(data, features), currBest)
+        currFeat = 0
+        for j in range(1, featureNum):
+            if j in features:
+                continue
+            acc = nearest(data, features + [j])
+            print("Feature(s) %s, the accuracy is %.2f%%" % (str(features + [j]), acc * 100))
+            if acc > currBest:
+                currBest = acc
+                currFeat = j
+        features.append(currFeat)
+        print("")
+        print("Feature set %s is the best with an accuracy of %.2f%%" % (str(features), currBest * 100))
+        if currBest > bestAcc:
+            bestAcc = currBest
+            bestFeatures = copy.deepcopy(features)
+        else:
+            print("!!!Accuracy is decreasing!!!")
+        print("")        
             
-            
-    
     file.close()
+    print("Finished! The best feature set is %s with an accuracy of %.2f%%" % (str(bestFeatures), bestAcc * 100))
+    
+def backward(fileName):
+    file = open(fileName, "r")
+    data = file.readlines()
+    featureNum = len(data[0].split())
+    features = list(range(1, featureNum))
+    bestFeatures = []
+    bestAcc = 0
+    acc = 0
+
+    print("The dataset has %d features and %d instances." % (featureNum - 1, len(data)))
+    print("")
+    
+    acc = nearest(data, features)
+    print("Feature(s) %s, the accuracy is %.2f%%" % (str(features), acc * 100))
+    print("")
+    print("Feature set %s is the best with an accuracy of %.2f%%" % (str(features), acc * 100))
+    print("")
+    
+    #Nested for loop that expand(delete feature) upon the (local)best feature set
+    for i in range(1, featureNum - 1):
+        currBest = 0
+        currFeat = 0
+        for j in range(1, featureNum):
+            if j not in features:
+                continue
+            temp = copy.deepcopy(features)
+            temp.remove(j)
+            acc = nearest(data, temp)
+            print("Feature(s) %s, the accuracy is %.2f%%" % (str(temp), acc * 100))
+            if acc > currBest:
+                currBest = acc
+                currFeat = j
+        features.remove(currFeat)
+        print("")
+        print("Feature set %s is the best with an accuracy of %.2f%%" % (str(features), currBest * 100))
+        if currBest > bestAcc:
+            bestAcc = currBest
+            bestFeatures = copy.deepcopy(features)
+        else:
+            print("!!!Accuracy is decreasing!!!")
+        print("")        
+            
+    file.close()
+    print("Finished! The best feature set is %s with an accuracy of %.2f%%" % (str(bestFeatures), bestAcc * 100))
 
 def main():
-    '''
+    
     print("Nearest Neighbor Classifier")
     fileName = input("Enter the name of the data file: ")
     
@@ -51,18 +118,11 @@ def main():
     print("1. Forward Selection")
     print("2. Backward Elimination")
     method = input("")
-    '''
     
-    '''
-    file = open("CS205_large_Data__6.txt", "r")
-    data = file.readlines()
-    features = [29, 4, 1]
-    #print(data[0].split())
-    
-    acc = nearest(data, features)
-    print(acc)
-    '''
-    
+    if method == '1':
+        forward(fileName)
+    else:
+        backward(fileName)
     
 if __name__ == "__main__":
     main()
